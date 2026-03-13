ccb68894b9be661552b19c4384a5f3c4107207ee
Adilson <adilsonchy@gmail.com>
2026-03-12 21:31:00 -0300

    feat: adiciona arquivos relacionados ao banco de dados

--- teste-basico/app/Http/Controllers/TesteController.php
+++ teste-basico/app/Http/Controllers/TesteController.php
@@ -1,13 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Tarefa;
 use Illuminate\Http\Request;
 
 class TesteController extends Controller
 {
-    public function exibir(){
-        $mensagem ='Isso veio do controller.';
-        return view('teste',['texto'=>$mensagem]);
+    public function exibir()
+    {
+        $mensagem = 'Isso veio do controller.';
+        return view('teste', ['texto' => $mensagem]);
+    }
+
+    public function criarTarefa()
+    {
+        $tarefa = new Tarefa();
+        $tarefa->titulo = "Aprender Laravel";
+        $tarefa->concluida = false;
+        $tarefa->save();
+
+        return "Tarefa salva com sucesso!";
     }
 }
--- teste-basico/database/migrations/2026_03_13_001008_create_tarefas_table.php
+++ teste-basico/database/migrations/2026_03_13_001008_create_tarefas_table.php
@@ -12,8 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tarefas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        $table->id();
+        $table->string('titulo'); // Coluna de texto
+        $table->boolean('concluida')->default(false); // Coluna booleana
+        $table->timestamps(); // Cria as colunas created_at e updated_at
         });
     }
 
